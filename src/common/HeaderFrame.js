f64023124e0f45e321e62b4990999bcd9ddf5415
warren <warrendlee1@gmail.com>
2020-11-21 15:18:53 -0800

    dashboard skeleton + header updates

--- src/common/HeaderFrame.js
+++ src/common/HeaderFrame.js
@@ -1,38 +1,97 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import { useState } from 'react';
+
 import MenuIcon from '@material-ui/icons/Menu';
+
+import { 
+  AppBar, 
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  makeStyles,
+  Popover,
+  Paper,
+  
+} from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flex: 1,
   },
+
   menuButton: {
-    marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(2),
   },
+
   title: {
     flexGrow: 1,
+    color:"white",
+    fontWeight:"bold"
   },
+
+  popoverPaper: {
+    minWidth: 300,
+    maxWidth: 300,
+    paddingTop: 15,
+    paddingBottom: 15,
+  },
+
+  popoverMenu: {
+    flex: 1, 
+    display:"flex",
+    flexDirection:"column",
+  }
 }));
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const [anchor, setAnchor] = useState(null)
+
+  const handleAvatarClick = event => {
+    setAnchor(event.currentTarget) // visually anchor the popup to the button
+  }
+
+  const handleAvatarClose = () => setAnchor(null)
+
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" className={classes.title}>
             Relab
           </Typography>
-          <Button color="inherit">Login</Button>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleAvatarClick}>
+            <MenuIcon />
+          </IconButton>
+          <Popover
+            classes={{
+              paper: classes.popoverPaper,
+            }}
+            anchorEl={anchor}
+            open={Boolean(anchor)}
+            onClose={handleAvatarClose}
+            anchorOrigin={{
+              // Location of the anchor on the button
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              // Anchor on the popover itself
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+          >
+            <div className={classes.popoverMenu}>
+              <Button>
+                FUNCTION 1
+              </Button>
+              <Button>
+                FUNCTION 2
+              </Button>
+            </div>
+
+
+          </Popover>
         </Toolbar>
       </AppBar>
     </div>
--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -12,24 +12,78 @@ import {
 } from '@material-ui/core'
 
 import HeaderFrame from '../../common/HeaderFrame.js'
+import monkey from '../../assets/images/monkey.jpg'
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        backgroundColor: "red",
+        display:"flex",
+        flexDirection:"column",
+        minHeight: '100vh',
+        // padding: 10,
+    },
 
+    profileContainer: {
+        marginTop: 10,
+        marginRight: 10,
+        marginLeft: 10,
+        minWidth: 1010,
+        maxHeight: 150,
+        flex: 0.25,
+        padding: 25,
+        display:"flex",
+        alignItems:"center",
+    },
+
+    avatar: {
+        minWidth: 100,
+        maxWidth: 100,
+        maxHeight: 100,
+        minHeight: 100,
+    },
+
+    body: {
+        display:"flex",
+        flexDirection:"row",
+        flex: 2,
+        margin: 10,
+
+    },
+
+    leftPanel: {
+        display:"flex",
+        flexDirection:"column",
+        flex: .2,
+        marginRight: 10,
+        minWidth: 250,
+        padding: 15,
+    },
+
+    rightPanel: {
+        display:"flex",
+        flex: 1,
+        minWidth: 750,
     },
 
 }),{ name: 'Dashboard' });
 
 export default function SignUp() {
   const classes = useStyles();
-
   return (
-    <Container className={classes.root}>
+    <div className={classes.root}>
         <HeaderFrame />
-        <Typography>
-            HELLO
-        </Typography>
-    </Container>
+        <Paper className={classes.profileContainer} elevation={2}>
+            <Avatar className={classes.avatar} alt="PROFILE" src={monkey} />
+        </Paper>
+        <div className={classes.body}>
+            <Paper className={classes.leftPanel} elevation={2}>
+                <Typography>Menu</Typography>
+                <Button>Courses</Button>
+                <Button>Progress</Button>
+            </Paper>
+            <Paper className={classes.rightPanel} elevation={2}>
+                RIGHT
+            </Paper>
+        </div>
+    </div>
   );
 }
