a409402c816117ccf15cf9dac8f6326fab169e00
warren <warrendlee1@gmail.com>
2020-11-28 14:46:23 -0800

    working on main

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import {
   Route,
   Link
 } from "react-router-dom";
-<<<<<<< HEAD
-import Register from './Register';
-import Login from './Login';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from './theme';
-
-=======
 import { ThemeProvider } from '@material-ui/core/styles';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -26,7 +18,6 @@ import Home from './pages/Dashboard/DashboardPages/Home.js';
 import Progress from './pages/Dashboard/DashboardPages/Progress.js';
 import Topic from './pages/Dashboard/DashboardPages/Topic.js';
 
->>>>>>> 6bbf63f6e21236e812ba2845e3235fae2481a970
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
@@ -36,48 +27,27 @@ export default function App() {
           <nav>
             <ul>
               <li>
-<<<<<<< HEAD
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/Register">Register</Link>
-=======
                 <Link to="/Dashboard/Home">Home</Link>
               </li>
               <li>
                 <Link to="/SignUp">Signup</Link>
->>>>>>> 6bbf63f6e21236e812ba2845e3235fae2481a970
               </li>
               <li>
                 <Link to="/Login">Login</Link>
               </li>
             </ul>
           </nav>
-<<<<<<< HEAD
-
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/Register">
-              <Register />
-=======
           {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
           <Switch>
             <Route path="/SignUp">
               <SignUp />
->>>>>>> 6bbf63f6e21236e812ba2845e3235fae2481a970
             </Route>
             <Route path="/Login">
               <Login />
             </Route>
-<<<<<<< HEAD
-            <Route path="/">
-              <Register />
-=======
             <Route path="/Dashboard/:id">
               <Dashboard />
->>>>>>> 6bbf63f6e21236e812ba2845e3235fae2481a970
             </Route>
           </Switch>
         </div>
@@ -86,19 +56,6 @@ export default function App() {
   );
 }
 
-<<<<<<< HEAD
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function Signup() {
-  return <h2>Register</h2>;
-}
-
-function Signin() {
-  return <h2>Login</h2>;
-}
-=======
 // function Home() {
 //   return <h2>Home</h2>;
 // }
@@ -110,4 +67,3 @@ function Signin() {
 // function Login() {
 //   return <h2>Login</h2>;
 // }
->>>>>>> 6bbf63f6e21236e812ba2845e3235fae2481a970
